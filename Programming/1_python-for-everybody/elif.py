e6390bdea4f045215986eb15d59c0c39fa4ed8bd
mariusrueve <marius.rueve@live.de>
2020-08-27 11:58:41 +0200

    Add elif.py

---
+++ Programming/1_python-for-everybody/elif.py
@@ -0,0 +1,10 @@
+x = 0
+if x < 2 :
+    print('Small')
+elif x < 10 :
+    print('Medium')
+else :
+    print('LARGE')
+print('All done')
+# if you have a block of if/ elif/ else statements, the code exits the block if
+# on statement gets triggered
