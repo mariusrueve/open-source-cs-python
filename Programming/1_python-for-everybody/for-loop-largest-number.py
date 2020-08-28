44a020a1caa5b3b9caa9e4c0db5fb4c951e45dfb
mariusrueve <marius.rueve@live.de>
2020-08-28 15:45:32 +0200

    Updated

--- Programming/1_python-for-everybody/for-loop-largest-number.py
+++ Programming/1_python-for-everybody/for-loop-largest-number.py
@@ -1,7 +1,9 @@
 number_list = [9, 41, 12, 3, 74, 15]
-largest_seen = int()
+largest_seen = None
 
 for number in number_list :
+    if largest_seen is None:
+        largest_seen = number
     if number > largest_seen :
         largest_seen = number
 
