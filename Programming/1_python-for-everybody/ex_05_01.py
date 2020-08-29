89585d37ee4552dbb4ff826334e2f657c72c5972
mariusrueve <marius.rueve@live.de>
2020-08-29 13:21:08 +0200

    Update try/ expect

--- Programming/1_python-for-everybody/ex_05_01.py
+++ Programming/1_python-for-everybody/ex_05_01.py
@@ -4,13 +4,21 @@ total = 0.0
 while True:
     user_input = input('Enter a number: ')
 
-    if user_input == 'done' :
+    # if user types "done" the script will exit the loop and print the results
+    if user_input == 'done':
         break
 
-    val = float(user_input)
-    print(val)
+    # try/ expect -> if input is invalid the loop starts again, no valies assigned
+    try:
+        val = float(user_input)
+    except:
+        print('Invalid input')
+        continue
+
+    # Adding the numbers for every loop
     num = num + 1
     total = total + val
 
-print('Done!')
+
+# printing the results
 print('Numbers:', num, 'Total:', total, 'Average:', total/num)
